31075280878c94d1bdbc4b0df8656a05dff509e5
João Luiz <jluizmineiro@gmail.com>
2023-05-06 15:57:57 -0300

    iniciando POO

--- OO/classe_data.dart
+++ OO/classe_data.dart
@@ -1,1 +1,13 @@
+class Data {
+  int? dia;
+  int? mes;
+  int? ano;
+}
 
+void main() {
+  var dataAniversario = Data();
+  dataAniversario.dia = 14;
+  dataAniversario.mes = 07;
+  dataAniversario.ano = 2002;
+  print("${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
+}
