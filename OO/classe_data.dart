96f7a0fccc939d462447fd8f497f728600712dfb
João Luiz <jluizmineiro@gmail.com>
2023-05-08 10:31:01 -0300

    construtores

--- OO/classe_data.dart
+++ OO/classe_data.dart
@@ -3,17 +3,27 @@ class Data {
   int? mes;
   int? ano;
 
+  // Data(int dia, int mes, int ano) {
+  //   this.dia = dia;
+  //   this.mes = mes;
+  //   this.ano = ano;
+  // }
+  Data(
+    this.dia,
+    this.mes,
+    this.ano,
+  );
+
   String obterFormatada() {
     return '$dia/$mes/$ano';
   }
 }
 
 void main() {
-  var dataAniversario = Data();
-  dataAniversario.dia = 14;
-  dataAniversario.mes = 07;
-  dataAniversario.ano = 2002;
-  // print("${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
+  var dataAniversario = Data(14, 07, 2002);
+  // dataAniversario.dia = 14;
+  // dataAniversario.mes = 07;
+  // dataAniversario.ano = 2002;
 
   String d1 = dataAniversario.obterFormatada();
   print(d1);
